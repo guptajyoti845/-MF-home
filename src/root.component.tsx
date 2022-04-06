0c0d6253b4e4523b939847c3dbcc3ed4e845b854
Jyoti Gupta <jyoti.gupta@thoughtworks.com>
2022-04-07 03:03:28 +0530

    add side navigation

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,13 @@
+import './side-bar.css'
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  // return <section>{props.name} is mounted!</section>;
+  return(
+    <div className='sidenav'>
+      <a href="/employees">Employees</a>
+      <a href="/">Admin</a>
+      <a href="/">Clients</a>
+      <a href="/">Contact</a>
+</div>
+  )
 }
